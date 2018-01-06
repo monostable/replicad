45b62df79fe5df8aed3484839101818028dc26c1
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-01-06 21:07:23 +0100

    Add Net, Circuit, Connection

--- examples/resistor_divider.js
+++ examples/resistor_divider.js
@@ -1,8 +1,11 @@
-let {Resistor} = require('../src')
+let {Resistor, Circuit, Net, Nets} = require('../src')
 let r1 = new Resistor('1k 0603')
 let r2 = r1.copy()
 r1.equals(r2) // true
-//let [vcc, vout, gnd] = Nets(3)
-//let circuit = Circuit()
+let [vcc, vout, gnd] = Nets(3)
+
+let circuit = new Circuit()
+circuit.connect(vcc, r1, r2)
 //circuit.connect_through(vcc, r1, vout, r2, gnd)
+console.log(circuit)
 //export circuit
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 class Resistor {
   constructor(description) {
-    this.copy.bind(this);
-    this.equals.bind(this);
     this.value = description;
   }
   copy() {
@@ -11,10 +9,41 @@ class Resistor {
     );
   }
   equals(other) {
-    const sameCons = this.constructor === other.constructor
+    const sameCons = this.constructor === other.constructor;
     const sameValue = () => this.value === other.value;
-    return sameCons && sameValue()
+    return sameCons && sameValue();
   }
 }
 
-module.exports = {Resistor};
+class Net {
+  constructor() {}
+}
+
+function Nets(n) {
+  return Array(n, null).map(() => new Net());
+}
+
+class Connection {
+  constructor(one, two) {
+    this.one = one
+    this.two = two
+  }
+}
+
+class Circuit {
+  constructor() {
+    this.connections = []
+  }
+  connect() {
+    Array.prototype.slice.call(arguments).forEach((x, i) => {
+      if (i > 0)  {
+        this._connect_two(x, arguments[i - 1])
+      }
+    })
+  }
+  _connect_two(one, two) {
+    this.connections.push(new Connection(one, two))
+  }
+}
+
+module.exports = {Resistor, Circuit, Net, Nets};
