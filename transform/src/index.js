3fd4499b9477026c6115ac533b572404fe5f7566
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-01-08 17:21:49 +0100

    Refactor by using babel-template

--- transform/src/index.js
+++ transform/src/index.js
@@ -1,13 +1,9 @@
 const {types: t} = require('@babel/core');
+const {default: template} = require('@babel/template');
 
 module.exports = function() {
   function log(message) {
-    const append = t.memberExpression(
-      t.identifier('__replicad__log__'),
-      t.identifier('append'),
-    );
-    const call = t.callExpression(append, [t.stringLiteral(message)]);
-    return t.expressionStatement(call);
+    return template.ast(`__replicad__log__.append("${message}")`);
   }
 
   const AddVarNamesToNets = {
@@ -18,10 +14,9 @@ module.exports = function() {
           const a = t.arrayExpression(names.map(s => t.stringLiteral(s)));
           path.node.arguments = [a];
         } else {
-          path
-            .parentPath
-            .parentPath
-            .replaceWith(log("'Nets' called without array pattern."));
+          path.parentPath.parentPath.replaceWith(
+            log("'Nets' called without array pattern."),
+          );
         }
       } else if (path.node.callee.name === 'Net') {
         console.log(path.parent.id.type);
@@ -29,12 +24,7 @@ module.exports = function() {
     },
   };
   function prependLog(path) {
-    const log = t.variableDeclaration('const', [
-      t.variableDeclarator(
-        t.identifier('__replicad__log__'),
-        t.arrayExpression(),
-      ),
-    ]);
+    const log = template.ast('const __replicad__log__ = []');
     path.node.body.unshift(log);
   }
   const visitor = {
