08f2be58076c6435af7b558720c025c9467fe853
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-01-08 23:36:14 +0100

    Figure out 'new' binding

--- examples/resistor_divider.js
+++ examples/resistor_divider.js
@@ -1,10 +1,11 @@
-let {Resistor, Circuit, Net, Nets} = require('../lib')
-let r1 = Resistor('1k 0603')
-let r2 = r1.copy()
+const {Resistor, Circuit, Net, Nets} = require('../lib')
+const r1 = Resistor('1k 0603')
+const r2 = r1.copy()
 r1.equals(r2) // true
-const [vcc, vout, gnd] = Nets()
+const [vcc, vout] = Nets()
+const gnd = Net()
 
-let circuit = Circuit()
+const circuit = Circuit()
 circuit.connect(vcc, r1, r2)
 //circuit.connect_through(vcc, r1, vout, r2, gnd)
 console.log(JSON.stringify(circuit, null, 2))
