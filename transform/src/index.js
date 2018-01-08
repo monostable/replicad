3228afac5d25db69dac8161754274722b13fd09b
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-01-08 14:25:16 +0100

    Implement adding arguments to Nets

--- transform/src/index.js
+++ transform/src/index.js
@@ -1,31 +1,23 @@
-//const babylon = require('babylon');
-//const fs = require('fs');
-//const {default: traverse} = require('babel-traverse');
-//
-//const example = fs.readFileSync('examples/resistor_divider.js', 'utf8');
-//
-//const ast = babylon.parse(example);
-//
-//const CallVisitor = {
-//  CallExpression(path) {
-//    const name = path.node.callee.name;
-//    if (name === 'Nets') {
-//      if (path.parent.id.type === 'ArrayPattern') {
-//        const names = path.parent.id.elements.map(x => x.name)
-//        console.log(names)
-//      }
-//    }
-//
-//  },
-//};
-//const VariableVisitor = {
-//  VariableDeclaration(path) {
-//    path.traverse(CallVisitor);
-//  },
-//};
-//
-//traverse(ast, VariableVisitor);
+const {types: t} = require('@babel/core');
 
 module.exports = function() {
-  return {}
-}
+  const AddVarNamesToNets = {
+    CallExpression(path) {
+      if (path.node.callee.name === 'Nets') {
+        if (path.parent.id.type === 'ArrayPattern') {
+          const names = path.parent.id.elements.map(x => x.name);
+          const a = t.arrayExpression(names.map(s => t.stringLiteral(s)));
+          path.node.arguments = [a];
+        }
+      }
+    },
+  };
+  const VariableVisitor = {
+    VariableDeclaration(path) {
+      path.traverse(AddVarNamesToNets);
+    },
+  };
+  return {
+    visitor: VariableVisitor,
+  };
+};
