6ff2a57d589b8bfdd83909ebebc64c6a3373c337
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-01-09 21:11:06 +0100

    Add initial Yosys netlist output

--- examples/resistor_divider.js
+++ examples/resistor_divider.js
@@ -1,12 +1,14 @@
 const {Resistor, Circuit, Net, Nets} = require('../lib')
 const r1 = Resistor('1k 0603')
-const r2 = r1.copy()
-r1.equals(r2) // true
+const r2 = Resistor('500 ohm 0603')
 const [vcc, vout] = Nets()
 const gnd = Net()
 
 const circuit = Circuit()
-circuit.connect(vcc, r1, r2)
+circuit.connect(vcc, r1)
+circuit.connect(r1.pin1, vout)
+circuit.connect(vout, r2)
+circuit.connect(r2.pin1, gnd)
 //circuit.connect_through(vcc, r1, vout, r2, gnd)
-console.log(circuit.toYosys())
+console.log(JSON.stringify(circuit.toYosys(), null, 2))
 //export circuit
