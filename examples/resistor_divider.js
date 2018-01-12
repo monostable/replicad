2e29d9276770610cb8560a625c679bcffe198315
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-01-12 22:03:17 +0100

    Make resistor_divider modular

--- examples/resistor_divider.js
+++ examples/resistor_divider.js
@@ -9,14 +9,21 @@ const {
   Input,
 } = require('../lib');
 
-const r1 = Resistor('1k 0603');
-const r2 = Resistor('500 ohm 0603');
+function resistor_divider(value1, value2) {
+  const r1 = Resistor(value1);
+  const r2 = Resistor(value2);
 
-const vcc = Power();
-const gnd = Ground();
+  const vcc = Power();
+  const gnd = Ground();
 
-const vout = Output();
+  const vout = Output();
 
-const circuit = Circuit();
-circuit.chain(vcc, r1, vout, r2, gnd);
+  const circuit = Circuit();
+  circuit.chain(vcc, r1, vout, r2, gnd);
+
+  return circuit;
+}
+
+const circuit = resistor_divider('1k', '500 ohm');
+console.log(circuit)
 console.log(JSON.stringify(circuit.toYosys(), null, 2));
