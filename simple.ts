64757c7e30fc7462d0d2db378cb7545b21af730a
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-05-25 20:58:58 +0200

    Change simple example to typescript

--- simple.ts
+++ simple.ts
@@ -22,18 +22,18 @@ calls.forEach(call => {
       .getText() === 'Circuit'
   ) {
     call.getArguments().forEach(arg => {
-      let name = arg.getText()
+      let name = arg.getText();
       //if it's an object prop get the object name
       if (arg.getKind() === ts.SyntaxKind.PropertyAccessExpression) {
-        let e = arg.getExpression()
+        let e = arg.getExpression();
         while (e.getKind() === ts.SyntaxKind.PropertyAccessExpression) {
-           e = e.getExpression()
+          e = e.getExpression();
         }
-        name = e.getText()
+        name = e.getText();
       }
-      arg.replaceWithText(`(${name}.name = '${name}', ${arg.getText()})`)
+      arg.replaceWithText(`(${name}.name = '${name}', ${arg.getText()})`);
     });
   }
 });
 
-f.copyImmediately('out.ts', {overwrite: true})
+f.copyImmediately('out.ts', {overwrite: true});
