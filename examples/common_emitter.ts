6588167b5ba85fb331b67d3ee684b7f33273b4ce
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-06-01 22:36:18 +0100

    Improve right variable finding in chain/connect

--- examples/common_emitter.ts
+++ examples/common_emitter.ts
@@ -9,17 +9,20 @@ import {
 } from "../lib/index"
 
 const q1 = new NPN()
-const r1 = new Resistor('1k')
+const r1 = new Resistor("1k")
 
 const circuit = new Circuit()
 
 const vcc = new Label()
 const vin = new Input()
 const vout = new Output()
-const gnd = new Ground()
+
+const gnds = {
+  one: new Ground()
+}
 
 circuit.chain(vcc, r1, vout, q1[0])
 circuit.connect(vin, q1[1])
-circuit.connect(q1[2], gnd)
+circuit.connect(q1[2], gnds.one)
 
 export default circuit
